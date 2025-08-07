16a3595695f7b40dd256f5ba8330db040c8bebe7
Aniket Nitnaware <nitnawareaniket167@gmail.com>
2025-08-07 23:40:47 +0530

    create controller - authControl

--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -1,40 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const userModel = require("../models/user-model");
-const bcrypt = require("bcrypt");
-const jwt = require("jsonwebtoken");
+const {registerUser} = require("../controllers/authControl")
 
 router.get('/', (req, res) => {
     res.send('Hey its working!');
 })
 
-router.post('/register', (req, res) => {
-    try {
-        let { email, password, fullname } = req.body
-
-        bcrypt.genSalt(10, function (err, salt) {
-            bcrypt.hash(password, salt, async function (err, hash) {
-                if (err) return res.send(err.message);
-                else {
-                    let user = await userModel.create(
-                        {
-                            email,
-                            password: hash,
-                            fullname,
-                        }
-                    );
-                    let token = jwt.sign({email,id: user._id}, "heyheyheye");
-                    res.cookie("token", token);
-                    res.send("user created successfully");
-                };
-            });
-        });
-
-
-    } catch (err) {
-        res.send(err.message);
-
-    }
-})
+router.post('/register', registerUser)
 
 module.exports = router;
